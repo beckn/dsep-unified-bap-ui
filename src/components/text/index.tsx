63bf0f2da733954a3997a7cb699a05314a0a1e62
Manoranjan Sahoo <manoranjansahoo@Manoranjans-MacBook-Pro.local>
2023-01-25 15:27:08 +0530

    adding custom fonts

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,3 +1,4 @@
+import {Fonts} from '@styles/fonts';
 import React, {FunctionComponent} from 'react';
 import {Text as RNText, TextStyle} from 'react-native';
 
@@ -5,15 +6,17 @@ type TextProps = {
   children: string | JSX.Element;
   style?: TextStyle | TextStyle[];
   onPress?: () => void;
+  fontFamily?: string;
 };
 
 export const Text: FunctionComponent<TextProps> = ({
   children,
   style,
   onPress,
+  fontFamily = Fonts.family.OPEN_SANS_REGULAR,
 }) => {
   return (
-    <RNText style={style} onPress={onPress}>
+    <RNText style={[style, {fontFamily: fontFamily}]} onPress={onPress}>
       {children}
     </RNText>
   );
