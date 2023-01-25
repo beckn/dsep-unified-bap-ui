0e8ee0c0ca41adda55cfd8e34938b992918c0f9c
Manoranjan Sahoo <manoranjansahoo@Manoranjans-MacBook-Pro.local>
2023-01-25 15:40:11 +0530

    adding demo code to use different font styles

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -4,11 +4,20 @@ import {Navigation} from '@interfaces/commonInterfaces';
 import {Text} from '@components/text';
 import {getString} from '@i18n';
 import {commonStyles} from '@styles/commonStyles';
+import {Fonts} from '@styles/fonts';
 
 function HomeScreen({navigation}: {navigation: Navigation}) {
   return (
     <View>
       <Text style={commonStyles.baseFontSize}>{getString('key1')}</Text>
+      <Text
+        fontFamily={Fonts.family.DM_SANS_REGULAR}
+        style={{fontSize: 16, color: 'red'}}>
+        This is DMSans-Regular
+      </Text>
+      <Text style={{fontSize: 16, color: 'green'}}>
+        This is OpenSans-Regular
+      </Text>
       <Button
         title="Go to Profile"
         onPress={() => navigation.navigate('Profile')}
