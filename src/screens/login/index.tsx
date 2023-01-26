f231e2d7fe70a230b8fdde12ee0b568834e1d368
Kumar Gaurav <kumargaurav@Kumars-MacBook-Air.local>
2023-01-26 06:33:36 +0530

    Adding google sign in capability for android and ios.

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Button} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 function LoginScreen({navigation}) {
+  useEffect(() => {
+    GoogleSignin.configure({});
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    console.log('user auth state changed', user);
+    setUser(user);
+    if (initializing) {
+      setInitializing(false);
+    }
+  }
+
+  async function onGoogleButtonPress() {
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+    console.log('idtoken', idToken);
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
+  const onGoogleSignOut = () => {
+    GoogleSignin.signOut()
+      .then(res => {
+        console.log('signed out', res);
+      })
+      .catch(err => {
+        console.log('sign out error', err);
+      });
+  };
   return (
     <View>
       <Text>LoginScreen</Text>
       <Button
         title="Go to Home"
         onPress={() => navigation.navigate('Dashboard')}
+      />
+      <Button title="Google Sign-Out" onPress={onGoogleSignOut} />
+      <Button
+        title="Google Sign-In"
+        onPress={() =>
+          onGoogleButtonPress().then(() =>
+            console.log('Signed in with Google!'),
+          )
+        }
       />
     </View>
   );
