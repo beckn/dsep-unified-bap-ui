d531bd9b34505901916fc5a37709bbb1cafe9983
Manoranjan Sahoo <manoranjansahoo@Manoranjans-MacBook-Pro.local>
2023-01-28 13:49:39 +0530

    adding tabs mentioned on figma

--- src/components/SvgIcon/index.tsx
+++ src/components/SvgIcon/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { View, ViewStyle, StyleSheet } from "react-native";
+import React from 'react';
+import {View, ViewStyle, StyleSheet} from 'react-native';
 import Filter from '../../assets/svg/filter.svg';
 import Search from '../../assets/svg/search.svg';
 import Remove from '../../assets/svg/remove.svg';
 import Star from '../../assets/svg/star.svg';
-import { Colors } from "@styles/colors";
-
+import Home from '../../assets/svg/home.svg';
+import Applied from '../../assets/svg/applied.svg';
+import Saved from '../../assets/svg/saved.svg';
+import Profile from '../../assets/svg/profile.svg';
+import {Colors} from '@styles/colors';
 
 export const SVGIcon = (props: PropTypes): JSX.Element => {
   const modifiedProps = {
@@ -14,7 +17,7 @@ export const SVGIcon = (props: PropTypes): JSX.Element => {
     style: null,
     fill: props.fill,
   };
-  const getIcon = (iconName: ICONS): JSX.Element => {
+  const getIcon = (iconName: ICONS): JSX.Element | undefined => {
     switch (iconName) {
       case ICONS.IC_FILTER:
         return <Filter {...modifiedProps} />;
@@ -24,6 +27,14 @@ export const SVGIcon = (props: PropTypes): JSX.Element => {
         return <Remove {...modifiedProps} />;
       case ICONS.IC_STAR:
         return <Star {...modifiedProps} />;
+      case ICONS.IC_HOME_FILLED:
+        return <Home {...modifiedProps} />;
+      case ICONS.IC_APPLIED:
+        return <Applied {...modifiedProps} />;
+      case ICONS.IC_SAVED:
+        return <Saved {...modifiedProps} />;
+      case ICONS.IC_PROFILE:
+        return <Profile {...modifiedProps} />;
     }
   };
 
@@ -58,4 +69,8 @@ export enum ICONS {
   IC_SEARCH = 'IC_SEARCH',
   IC_REMOVE = 'IC_REMOVE',
   IC_STAR = 'IC_STAR',
+  IC_HOME_FILLED = 'IC_HOME_FILLED',
+  IC_APPLIED = 'IC_APPLIED',
+  IC_SAVED = 'IC_SAVED',
+  IC_PROFILE = 'IC_PROFILE',
 }
