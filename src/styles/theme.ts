332a8c58411c70663e76727a4b4a874a69491aa2
Manoranjan Sahoo <manoranjansahoo@Manoranjans-MacBook-Pro.local>
2023-01-30 00:16:22 +0530

    adding theming

---
+++ src/styles/theme.ts
@@ -0,0 +1,32 @@
+export const theme = (colorScheme?: string) =>
+  colorScheme === 'dark'
+    ? {
+        white: '#FFFFFF',
+        black: 'green',
+        black10: 'rgba(0, 0, 0, 0.1)',
+        background: '#E5E5E5',
+        grey: 'grey',
+        silverSand: '#C4C4C4',
+        cardTitle: '#171716',
+        cardDesc: 'rgba(23, 23, 22, 0.75)',
+        dimGray: 'rgba(104, 104, 104, 0.2)',
+        oliveBlack: '#3D3D3D',
+        detailHeaderBg: '#E0E0E0',
+        lightGray: '#F9F9F9',
+        cardDescription: 'rgba(23, 23, 22, 0.75)',
+      }
+    : {
+        white: '#FFFFFF',
+        black: '#000',
+        black10: 'rgba(0, 0, 0, 0.1)',
+        background: '#E5E5E5',
+        grey: 'grey',
+        silverSand: '#C4C4C4',
+        cardTitle: '#171716',
+        cardDesc: 'rgba(23, 23, 22, 0.75)',
+        dimGray: 'rgba(104, 104, 104, 0.2)',
+        oliveBlack: '#3D3D3D',
+        detailHeaderBg: '#E0E0E0',
+        lightGray: '#F9F9F9',
+        cardDescription: 'rgba(23, 23, 22, 0.75)',
+      };
